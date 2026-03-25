4addeef3bc6979d7fcc09905c9f62a963458afa8
r-happy <106812882+r-happy@users.noreply.github.com>
2026-03-25 17:27:48 +0900

    sync tmux, wezterm

--- wezterm/tawny.lua
+++ wezterm/tawny.lua
@@ -8,27 +8,27 @@ return {
     cursor_bg     = "#f0d3ae",
     cursor_border = "#f0d3ae",
     cursor_fg     = "#171310",
-    selection_bg  = "#221e1b",
+    selection_bg  = "#3a2e28",
     selection_fg  = "#f0d3ae",
 
     ansi = {
       "#171310", -- black
-      "#b85c4a", -- red
-      "#6a8c55", -- green
+      "#c05868", -- red
+      "#7a9860", -- green
       "#ddb46e", -- yellow
-      "#7090a0", -- blue
+      "#6888a8", -- blue
       "#a48494", -- magenta
-      "#5a8070", -- cyan
+      "#5e8c7a", -- cyan
       "#f0d3ae", -- white
     },
     brights = {
       "#3a2c26", -- bright black
-      "#b85c4a", -- bright red
-      "#6a8c55", -- bright green
+      "#c05868", -- bright red
+      "#7a9860", -- bright green
       "#ddb46e", -- bright yellow
-      "#7090a0", -- bright blue
+      "#6888a8", -- bright blue
       "#a48494", -- bright magenta
-      "#5a8070", -- bright cyan
+      "#5e8c7a", -- bright cyan
       "#f0d3ae", -- bright white
     },
 
@@ -40,7 +40,7 @@ return {
       },
       inactive_tab = {
         bg_color  = "#14100d",
-        fg_color  = "#7a6650",
+        fg_color  = "#9a7f68",
       },
       inactive_tab_hover = {
         bg_color  = "#221e1b",
@@ -48,7 +48,7 @@ return {
       },
       new_tab = {
         bg_color  = "#14100d",
-        fg_color  = "#7a6650",
+        fg_color  = "#9a7f68",
       },
       new_tab_hover = {
         bg_color  = "#221e1b",
