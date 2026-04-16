5aedc6e981b8f74baf15db93fca226f62b3729ea
r-happy <106812882+r-happy@users.noreply.github.com>
2026-04-16 15:48:20 +0900

    change

--- lua/tawny/highlights/syntax.lua
+++ lua/tawny/highlights/syntax.lua
@@ -1,50 +1,70 @@
 local M = {}
 
+local function link(group)
+  return { link = group }
+end
+
 function M.get(c, opts)
+  opts = opts or {}
+
+  local italic_comments = opts.italic_comments ~= false
+  local italic_keywords = opts.italic_keywords ~= false
+  local bold_todo = opts.bold_todo ~= false
+
   return {
-    -- コメント
-    Comment        = { fg = c.fg_dim, italic = true },
-    -- 定数
+    ----------------------------------------------------------------
+    -- ベース実体
+    ----------------------------------------------------------------
+    Comment        = { fg = c.fg_dim, italic = italic_comments },
+
     Constant       = { fg = c.yellow },
     String         = { fg = c.green },
-    Character      = { fg = c.green },
     Number         = { fg = c.orange },
-    Float          = { fg = c.orange },
-    Boolean        = { fg = c.orange },
-    -- 識別子
+
     Identifier     = { fg = c.fg },
-    Variable       = { fg = c.fg },
     Function       = { fg = c.blue },
-    -- 文・制御
+
     Statement      = { fg = c.violet },
-    Conditional    = { fg = c.violet, italic = true },
-    Repeat         = { fg = c.violet },
-    Label          = { fg = c.violet },
-    Keyword        = { fg = c.violet, italic = true },
+    Keyword        = { fg = c.violet, italic = italic_keywords },
     Exception      = { fg = c.red },
     Operator       = { fg = c.fg_dim },
-    -- プリプロセッサ
+
     PreProc        = { fg = c.teal },
-    Include        = { fg = c.teal },
-    Define         = { fg = c.teal },
-    Macro          = { fg = c.teal },
-    PreCondit      = { fg = c.teal },
-    -- 型
     Type           = { fg = c.teal },
-    StorageClass   = { fg = c.teal },
-    Structure      = { fg = c.teal },
-    Typedef        = { fg = c.teal },
-    -- 特殊
+
     Special        = { fg = c.yellow },
-    SpecialChar    = { fg = c.yellow },
     Tag            = { fg = c.blue },
-    Delimiter      = { fg = c.border },
-    SpecialComment = { fg = c.fg_dim, italic = true },
-    Debug          = { fg = c.red },
-    -- その他
-    Underlined     = { underline = true },
+    Delimiter      = { fg = c.fg_dim },
+
     Error          = { fg = c.red },
-    Todo           = { fg = c.bg, bg = c.yellow, bold = true },
+    Todo           = { fg = c.bg, bg = c.yellow, bold = bold_todo },
+    Underlined     = { underline = true },
+
+    ----------------------------------------------------------------
+    -- エイリアス
+    ----------------------------------------------------------------
+    Character      = link("String"),
+    Float          = link("Number"),
+    Boolean        = link("Number"),
+
+    Variable       = link("Identifier"),
+
+    Conditional    = link("Keyword"),
+    Repeat         = link("Statement"),
+    Label          = link("Statement"),
+
+    Include        = link("PreProc"),
+    Define         = link("PreProc"),
+    Macro          = link("PreProc"),
+    PreCondit      = link("PreProc"),
+
+    StorageClass   = link("Type"),
+    Structure      = link("Type"),
+    Typedef        = link("Type"),
+
+    SpecialChar    = link("Special"),
+    SpecialComment = link("Comment"),
+    Debug          = link("Error"),
   }
 end
 
