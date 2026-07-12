f6cb29e46bde81649ea628ffe808288bb66d8926
r-happy <106812882+r-happy@users.noreply.github.com>
2026-07-12 10:38:11 +0900

    update

--- wezterm/tawny.lua
+++ wezterm/tawny.lua
@@ -5,53 +5,53 @@
 return {
   colors = {
     ansi = {
-      "#0e0e0d",
-      "#d2707d",
-      "#adc497",
-      "#d9c18c",
-      "#97b4ce",
-      "#c0a5c0",
-      "#99c2b5",
-      "#a79481"
+      "#171514",
+      "#d56e78",
+      "#9db672",
+      "#d8b66d",
+      "#6dabdf",
+      "#ae97d8",
+      "#73b598",
+      "#a69585"
     },
-    background = "#151414",
+    background = "#221f1d",
     brights = {
-      "#81756a",
-      "#d2707d",
-      "#adc497",
-      "#d9c18c",
-      "#97b4ce",
-      "#c89daf",
-      "#9ac5c6",
-      "#ede4d4"
+      "#796f67",
+      "#d56e78",
+      "#9db672",
+      "#d8b66d",
+      "#6dabdf",
+      "#cd83ad",
+      "#6fb9c1",
+      "#f1e6d6"
     },
-    cursor_bg = "#d9c3a6",
-    cursor_border = "#d9c3a6",
-    cursor_fg = "#151414",
-    foreground = "#c9baa6",
-    selection_bg = "#332d29",
-    selection_fg = "#c9baa6",
+    cursor_bg = "#e0c6a3",
+    cursor_border = "#e0c6a3",
+    cursor_fg = "#221f1d",
+    foreground = "#cbbba9",
+    selection_bg = "#3f2f24",
+    selection_fg = "#cbbba9",
     tab_bar = {
       active_tab = {
-        bg_color = "#151414",
-        fg_color = "#c9baa6"
+        bg_color = "#221f1d",
+        fg_color = "#cbbba9"
       },
-      background = "#121211",
+      background = "#1d1a19",
       inactive_tab = {
-        bg_color = "#121211",
-        fg_color = "#81756a"
+        bg_color = "#1d1a19",
+        fg_color = "#796f67"
       },
       inactive_tab_hover = {
-        bg_color = "#242322",
-        fg_color = "#c9baa6"
+        bg_color = "#342e2a",
+        fg_color = "#cbbba9"
       },
       new_tab = {
-        bg_color = "#121211",
-        fg_color = "#81756a"
+        bg_color = "#1d1a19",
+        fg_color = "#796f67"
       },
       new_tab_hover = {
-        bg_color = "#242322",
-        fg_color = "#c9baa6"
+        bg_color = "#342e2a",
+        fg_color = "#cbbba9"
       }
     }
   }
